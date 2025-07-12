b293f0671b38882fe062ad7f77a3bfbb8cc503ab
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-07-12 15:05:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const inputs = document.getElementById('code-container');
+inputs.addEventListener('input',(e) => {
+	e.preventDefault();
+	let input = e.target;
+
+	if (!/^[0-9]?$/.test(input.value)) {
+		input.value = "";
+		return;
+	}
+
+	if (input.value && input.nextElementSibling) {
+		input.nextElementSibling.focus();
+	}
+})
+
+inputs.addEventListener('keydown', (event) => {
+    const input = event.target;
+    if (input.tagName !== 'INPUT') return;
+
+    
+    if (event.key === 'Backspace' && input.value === '') {
+      if (input.previousElementSibling) {
+        input.previousElementSibling.focus();
+      }
+    }
+  });
+
